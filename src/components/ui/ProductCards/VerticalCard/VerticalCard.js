fc9ed480c1204450fb7d3cda2b4a876c2637780a
SaifullaJubair <saifullajubair6@gmail.com>
2024-03-01 10:54:07 +0600

    vertical card done

--- src/components/ui/ProductCards/VerticalCard/VerticalCard.js
+++ src/components/ui/ProductCards/VerticalCard/VerticalCard.js
@@ -1,25 +1,30 @@
-import { FaStar } from "react-icons/fa6";
+import { FaArrowDown, FaChevronDown, FaStar } from "react-icons/fa6";
 import pic1 from "../../../../assets/pic.png";
 import Image from "next/image";
+import { CiTrophy } from "react-icons/ci";
 const VerticalCard = () => {
   return (
-    <div className="bg-white rounded-xl my-6 py-4 ">
+    <div className="bg-white rounded-xl my-8 py-4 ">
+      {/* card side section index no */}
       <div className="relative">
         <div className="absolute -top-8 -left-2 bg-org  py-2 px-4 rounded-r-lg">
-          <p className="text-white text-xs">Best Choice</p>
+          <p className="text-white text-xs flex gap-2 items-center ">
+            <CiTrophy className="text-white  text-lg" />
+            Best Choice
+          </p>
         </div>
-        <div className="absolute  -left-2 mt-4 bg-white  px-4 py-2 rounded-full border-[#E1E4E6] border-[1px]">
+        <div className="absolute  -left-5 mt-4 bg-white  px-4 py-2 rounded-full border-[#E1E4E6] border-[1px]">
           2
         </div>
       </div>
-      <div className="grid grid-cols-12 gap-2">
+      <div className="grid grid-cols-12  gap-2">
         {/* Img section */}
 
-        <div className="col-span-3 mx-auto flex items-center justify-center">
+        <div className="  md:col-span-3  mx-auto flex items-center justify-center">
           <Image height={110} width={141} src={pic1} alt="" />
         </div>
         {/* details section */}
-        <div className="col-span-6 mx-2">
+        <div className="md:col-span-6 mx-2">
           <div className=" ">
             <p className="text-darkLightText ">
               <span className="text-darkText font-bold">
@@ -29,50 +34,19 @@ const VerticalCard = () => {
               Comprehensive Digital Platform Creation Tool, Streamlined Design
               Interface for Professional Websites and Online Stores (Black/Blue)
             </p>
-            <p className="text-darkLightText ">
-              <span className="text-darkText font-bold">
-                {" "}
-                WixPro 72-Inch Responsive Website Builder-
-              </span>
-              Comprehensive Digital Platform Creation Tool, Streamlined Design
-              Interface for Professional Websites and Online Stores (Black/Blue)
+            <h3 className="text-darkText font-bold py-2">Main highlights</h3>
+            <p className="pl-3 text-darkLightText py-1.5">
+              [What You Get]: Gain access to the SiteCraft design studio,
+              featuring a robust selection of design elements, SEO optimization
+              tools, and e-commerce integrations.
             </p>
-            <p className="text-darkLightText ">
-              <span className="text-darkText font-bold">
-                {" "}
-                WixPro 72-Inch Responsive Website Builder-
-              </span>
-              Comprehensive Digital Platform Creation Tool, Streamlined Design
-              Interface for Professional Websites and Online Stores (Black/Blue)
-            </p>
-            <p className="text-darkLightText ">
-              <span className="text-darkText font-bold">
-                {" "}
-                WixPro 72-Inch Responsive Website Builder-
-              </span>
-              Comprehensive Digital Platform Creation Tool, Streamlined Design
-              Interface for Professional Websites and Online Stores (Black/Blue)
-            </p>
-            <p className="text-darkLightText ">
-              <span className="text-darkText font-bold">
-                {" "}
-                WixPro 72-Inch Responsive Website Builder-
-              </span>
-              Comprehensive Digital Platform Creation Tool, Streamlined Design
-              Interface for Professional Websites and Online Stores (Black/Blue)
-            </p>
-            <p className="text-darkLightText ">
-              <span className="text-darkText font-bold">
-                {" "}
-                WixPro 72-Inch Responsive Website Builder-
-              </span>
-              Comprehensive Digital Platform Creation Tool, Streamlined Design
-              Interface for Professional Websites and Online Stores (Black/Blue)
-            </p>
+            <button className="flex gap-2 items-center pl-1 pt-2 text-primary hover:text-blue-400 hover:underline">
+              Show more <FaChevronDown />
+            </button>
           </div>
         </div>
         {/* review and vew section */}
-        <div className="col-span-3 mx-2 flex item-center flex-col justify-between">
+        <div className="md:col-span-3 mx-2 flex item-center flex-col justify-between">
           {/* rating section */}
           <div className="pb-4 ">
             <div className="rounded-b-xl bg-[#F3F9FF] px-4 -mt-4 pb-2 flex flex-col items-center justify-center w-[135px] mx-auto">
